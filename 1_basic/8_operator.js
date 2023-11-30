15d383b81fb4f3d8137cbf8c45f16b4023ac8541
Aan Budi Setiawan <setiawan.aanbudi@gmail.com>
2023-11-30 20:53:55 +0700

    operator

---
+++ 1_basic/8_operator.js
@@ -0,0 +1,16 @@
+console.log(12 == 12)
+console.log(2 > 12)
+console.log(65 < 12)
+console.log(12 === "12")
+console.log(12 == "12")
+
+// logical
+
+console.log(true && true)// true
+console.log(false && true)// false
+console.log(false && false)// false
+
+console.log(true || true) // true
+console.log(true || false) // true
+console.log(false || true) // true
+console.log(false || false) // false
