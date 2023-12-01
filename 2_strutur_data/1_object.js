42ede6d95afa2e907743b81debbb1a9eab17c399
Aan Budi Setiawan <setiawan.aanbudi@gmail.com>
2023-12-01 21:25:18 +0700

    object

---
+++ 2_strutur_data/1_object.js
@@ -0,0 +1,17 @@
+const car = {
+    name : "aan",
+    age : 19,
+    address : "Jakarta",
+    "a_asa" : "a_asa",
+    class : "Semester",
+}
+
+// car = {name: "budi", age: 20}
+
+console.log(car.address)
+console.log(car.age)
+car.age = car.age + 10
+console.log(car.name)
+console.log(car["class"])
+console.log(car.a_asa)
+console.log(car)
